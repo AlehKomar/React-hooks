387e27a12411efcd35342700c678e4603459f18c
Aleh Komar <komar.aleh@gmail.com>
2023-09-12 17:43:14 +0200

    80

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,22 @@
-const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateBasics;
+const UseStateBasics = () => {
+  // const value = useState('hello')[0]
+  // const func = useState('hello')[1]
+  // console.log(value)
+  // console.log(func)
+  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    setCount((prev) => prev + 1)
+  }
+  return (
+    <div>
+      <h2>You clicked {count} times</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        Increase
+      </button>
+    </div>
+  )
+}
+
+export default UseStateBasics
