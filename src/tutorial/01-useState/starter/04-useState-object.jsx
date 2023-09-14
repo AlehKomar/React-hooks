e25c641b27646cbb06711386b764bbaab57ff120
Aleh Komar <komar.aleh@gmail.com>
2023-09-14 14:36:43 +0200

    88

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,25 +1,28 @@
 import { useState } from 'react'
 
 const UseStateObject = () => {
-  const person = { name: 'Peter', age: 25, hobby: 'running' }
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 25,
+    hobby: 'running'
+  })
   const [name, setName] = useState('Peter')
   const [age, setAge] = useState(25)
   const [hobby, setHobby] = useState('running')
 
   const changePerson = () => {
-    setName('John')
-    setAge(30)
-    setHobby('racing')
+    setPerson({ name: 'John', age: 30, hobby: 'racing' })
+    //setPerson({ ...person, hobby: 'racing' })
   }
   return (
     <>
       <div>
-        <h3>{name}</h3>
-        <h3>{age}</h3>
-        <h3>Enjoys: {hobby}</h3>
+        <h3>{person.name}</h3>
+        <h3>{person.age}</h3>
+        <h3>Enjoys: {person.hobby}</h3>
       </div>
       <div>
-        <button type='button' className='btn' onClick={() => changePerson()}>
+        <button type='button' className='btn' onClick={changePerson}>
           Change
         </button>
       </div>
