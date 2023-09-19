3dde9dbd193ab9c6d73ec39b757e91ca54fe912a
Aleh Komar <komar.aleh@gmail.com>
2023-09-19 13:15:02 +0200

    111

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,41 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+  const [user, setUser] = useState(null)
+
+  const login = () => {
+    setUser({ name: 'vegan food truck' })
+  }
+
+  const logout = () => {
+    setUser(null)
+  }
+  console.log(user)
+  let text = ''
+  if (user) {
+    text = `hello there, ${user.name}`
+  }
+  return (
+    <div>
+      {user ? (
+        <UserDiv text={text} onClick={logout} buttonLabel='logout' />
+      ) : (
+        <UserDiv text='please login' onClick={login} buttonLabel='login' />
+      )}
+    </div>
+  )
+}
+
+export default UserChallenge
+
+const UserDiv = (prop) => {
+  const { text, onClick, buttonLabel } = prop
+  return (
+    <div>
+      <h4>{text}</h4>
+      <button className='btn' onClick={onClick}>
+        {buttonLabel}
+      </button>
+    </div>
+  )
+}
